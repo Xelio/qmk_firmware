c7ff12dde08de16ed1ad8163ea96abec38cfe0da
Xelio Cheong <xelio.cheong@gmail.com>
2022-03-28 00:56:18 +0800

    add quote and semicolon to symbol layout

--- keyboards/lily58/keymaps/xelio/keymap.c
+++ keyboards/lily58/keymaps/xelio/keymap.c
@@ -133,7 +133,6 @@ typedef struct {
 sTapHolds th_keycodes[] = {
     S_TAP_HOLD(LT(0,KC_MINS), 0, KC_EQL),// tap -, hold =
     S_TAP_HOLD(LT(0,KC_SLSH), 0, KC_BSLS),// tap slash, hold backslash
-    S_TAP_HOLD(LT(0,KC_BSLS), 0, KC_SLSH), // tap backslash, hold slash
     S_TAP_HOLD(LT(0,KC_LPRN), KC_LPRN, KC_LABK), // tap (, hold <
     S_TAP_HOLD(LT(0,KC_RPRN), KC_RPRN, KC_RABK), // tap ), hold >
     S_TAP_HOLD(W_H1, 0, KC_1),
@@ -223,9 +222,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   !  |   @  |   {  |   }  |   &  |                    |   +  |  7   |   8  |   9  |   *  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   #  |   $  |(    <|)    >|\|  /?|-------.    ,-------|   =+ |  4   |   5  |   6  |   .  |      |
+ * |      |   #  |   $  |(    <|)    >|  \|  |-------.    ,-------|   =+ |  4   |   5  |   6  |   .  |  '"  |
  * |------+------+------+------+------+------|   `~  |    | CALC  |------+------+------+------+------+------|
- * |      |   %  |   ^  |   [  |   ]  |   _  |-------|    |-------|   -  |  1   |   2  |   3  |   /  |      |
+ * |      |   %  |   ^  |   [  |   ]  |   _  |-------|    |-------|   -  |  1   |   2  |   3  |   /? |  ;:  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Enter  /       \Space \  |RAISE |   0  |   0  |
  *                   |      |      |      |/       /         \      \ |      | AltGr| RGUI |
@@ -234,8 +233,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   CLR_LY,  KC_F1,   KC_F2,   KC_F3,         KC_F4,         KC_F5,                            KC_F6,   KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11, \
   _______, KC_EXLM, KC_AT,   KC_LCBR,       KC_RCBR,       KC_AMPR,                          KC_PPLS, KC_7,    KC_8,    KC_9,  KC_PAST, KC_F12, \
-  _______, KC_HASH, KC_DLR,  LT(0,KC_LPRN), LT(0,KC_RPRN), LT(0,KC_BSLS),                    KC_EQL,  LOWER_4, LOWER_5, LOWER_6,  KC_DOT,  _______, \
-  _______, KC_PERC, KC_CIRC, KC_LBRC,       KC_RBRC,       KC_UNDS,      KC_GRAVE,  KC_CALC, KC_PMNS, KC_1,    KC_2,    KC_3,  KC_PSLS, _______, \
+  _______, KC_HASH, KC_DLR,  LT(0,KC_LPRN), LT(0,KC_RPRN), KC_BSLS,                    KC_EQL,  LOWER_4, LOWER_5, LOWER_6,  KC_DOT,  KC_QUOT, \
+  _______, KC_PERC, KC_CIRC, KC_LBRC,       KC_RBRC,       KC_UNDS,      KC_GRAVE,  KC_CALC, KC_PMNS, KC_1,    KC_2,    KC_3,  KC_SLSH, KC_SCLN, \
                                         _______, _______, _______,         KC_ENT, KC_SPC,     _______, RALT_0,  RGUI_0 \
 ),
 /* ADJUST
@@ -538,7 +537,6 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     case GUI_SCLN:
     case LT(0,KC_MINS):
     case LT(0,KC_SLSH):
-    case LT(0,KC_BSLS):
     case LT(0,KC_LPRN):
     case LT(0,KC_RPRN):
     case W_H1:
